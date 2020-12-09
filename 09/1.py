273d11f7e2e3e25094480fb47adb79f23d0d5764
Alejandro Suarez <alejandroscf@gmail.com>
2020-12-09 16:13:23 +0100

    09: Solved part one and two

--- 09/1.py
+++ 09/1.py
@@ -3,13 +3,51 @@
 import sys
 import re
 
-PREAMBLE = 5
-numbers = []
+PREAMBLE = int(sys.argv[1])
+num_input = []
+weakness = 0
 
 for line in sys.stdin:
-    instructions.append(line.strip())
+     num_input.append(int(line.strip()))
+
 
 # Part one
 print('Executing part one')
-print(numbers)
+idx = 0
+numbers = []
+
+for curr_number in num_input:
+    #print(curr_number)
+    if idx >= PREAMBLE:
+        valid = False
+        for i in numbers:
+            for j in numbers:
+                if i + j == curr_number and i != j:
+                    #print("valid")
+                    valid = True
+                    break
+        if not valid:
+            print(curr_number)
+            weakness = curr_number
+            break
+        numbers.pop(0)
+    if weakness != 0:
+        break
+    idx += 1
+    numbers.append(curr_number)
+
+two_result = 0
+# Part two
+print('Executing part two')
+for i, num in enumerate(num_input):
+    for j in range(i+1, idx):
+        suma = sum(num_input[slice(i,j)])
+        if suma == weakness:
+            print(min(num_input[slice(i,j)]) + max(num_input[slice(i,j)]))
+            two_result = min(num_input[slice(i,j)]) + max(num_input[slice(i,j)])
+            break
+        elif suma > weakness:
+            break
+    if two_result != 0:
+        break
 
