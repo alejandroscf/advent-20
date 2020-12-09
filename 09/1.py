d4424105b3a34d7e2a0ad3d587997dc1e2010d8c
alejandroscf <alejandroscf@gmail.com>
2020-12-09 14:49:39 +0100

    09: Read input

---
+++ 09/1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+import re
+
+PREAMBLE = 5
+numbers = []
+
+for line in sys.stdin:
+    instructions.append(line.strip())
+
+# Part one
+print('Executing part one')
+print(numbers)
+
