0f1972e50e5616406f5b5e4b21ff488837df250c
Alejandro Suarez <alejandroscf@gmail.com>
2020-12-14 15:22:56 +0100

    14: Solved part two

--- 14/2.py
+++ 14/2.py
@@ -3,31 +3,36 @@
 import sys
 #import copy
 import re
+#import time
 
 file_lines = []
 memory = {}
 mask = {}
 
-def do_mask(mask, data):
-    #masked = data & mask[1]
-    masked = data | mask[0]
-    return masked
-
 def do_write(mask, addr, data):
     global memory
     addr = addr | mask[0]
-    expand(mask['X'], format(addr, '36b'))
+    #print(format(addr, '036b'))
+    #print(mask['X'])
+    expand(mask['X'], format(addr, '036b'), data)
 
 def expand(mask, addr, data):
     global memory
-    if mask == int(0):
+    #print('expand')
+    #print(mask)
+    #time.sleep(1)
+    if int(mask) == 0:
+        #print('fin')
         memory[int(addr, 2)] = data
     else:
         pos = mask.find('1')
-        addr[pos] = 0
-        expand(mask, addr)
-        addr[pos] = 1
-        expand(mask, addr)
+        mask = mask[:pos] + str(0) + mask[pos + 1:]
+        #print(mask)
+        addr = addr[:pos] + str(0) + addr[pos + 1:]
+        #print(addr)
+        expand(mask, addr, data)
+        addr = addr[:pos] + str(1) + addr[pos + 1:]
+        expand(mask, addr, data)
 
 
 
@@ -37,7 +42,7 @@ for line in sys.stdin:
         #mask[1] = int(data.replace('X', str(1)),2)
         mask[0] = int(data.replace('X', str(0)),2)
         mask['X'] = data.replace('1', str(0)).replace('X', str(1))
-        print(mask['X'])
+        #print(mask['X'])
     else:
         direction = int(re.findall('^mem\[(\d+)\]$', ins)[0])
         do_write(mask, direction, int(data))
